62ae0613e4a819e351d2f7def5b3803343c1c0ca
Rutwik <rutkalbandhe@gmail.com>
2023-04-04 13:20:55 -0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export const Navbar = () => {
+
+    return (
+        <>
+            <div className="navbar">
+                <h1>Google Translator</h1>
+            </div>
+        </>
+    );
+};
